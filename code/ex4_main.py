e4be7447fa6e610408c0ebfda43f54a80440660a
HoangNam <dinhhoangnam998@gmail.com>
2023-04-16 14:23:40 +0200

    :construction: working on 4.3

---
+++ code/ex4_main.py
@@ -0,0 +1,19 @@
+import grammar
+import parse
+import parser
+
+with open("../data/abnormal_grammar.srgs", "r") as f:
+    lines = f.readlines()
+    gr = grammar.Grammar(lines)
+print(gr)
+
+sentence = "c d g h f"
+tokens = sentence.split(" ")
+
+print("is_in_language", parser.is_in_language(tokens, gr))
+
+parsing_results = parser.parse(tokens, gr)
+for tree in parsing_results:
+    print(tree.to_dot(), '\n')
+# one of the parsing results should yield the same result as the example from above
+# assert repr(parser.example_telescope_parse()) in map(repr, parsing_results)
--- code/grammar.py
+++ code/grammar.py
@@ -8,10 +8,12 @@ class Symbol:
 
     terminal: bool
     symbol: str
+    original: bool
 
-    def __init__(self, symbol: str):
+    def __init__(self, symbol: str, original=True):
         self.terminal = not symbol.startswith("$")
         self.symbol = symbol if self.terminal else symbol[1:]
+        self.original = original
 
     def __repr__(self):
         return ("" if self.terminal else "$") + self.symbol
@@ -49,6 +51,7 @@ class Grammar:
     symbols: Mapping[str, Symbol] = {}  # map from strings to symbols
     # map from RHSs to the matching rules
     rule_map: Mapping[tuple, GrammarRule]
+    original_rules: List[GrammarRule] = []
 
     """initialize a new grammar from a srgs grammar file"""
 
@@ -72,12 +75,16 @@ class Grammar:
                 match.groups()) == 3, "cannot parse line {}".format(line)
             is_public = match.group(1) != ""
             lhs = self.get_symbol(match.group(2))
+            #
+            assert lhs.terminal is False, "a terminal can't produce anything {} !".format(lhs.symbol)
             rhs = [self.get_symbol(s)
                    for s in re.split(r"\s+", match.group(3))]
             rule = GrammarRule(lhs, rhs)
             self.rules.append(rule)
             if is_public:
                 self.start_symbol = lhs
+        self.original_rules = self.rules
+        self.normalize_rules()
         self.build_rule_map()
 
     def build_rule_map(self):
@@ -97,21 +104,64 @@ class Grammar:
                 [str(r) if r.lhs != self.start_symbol else "public " + str(r) for r in self.rules])
 
     def is_CNF(self):
-        return all([self._validate_CNF_rule(rule) for rule in self.rules])
+        return all([self._is_valid_CNF_rule(rule) for rule in self.rules])
 
-    def _validate_CNF_rule(self, rule: GrammarRule) -> bool:
+    def _is_valid_CNF_rule(self, rule: GrammarRule) -> bool:
         lhs, rhs, length = rule.lhs, rule.rhs, len(rule.rhs)
-        if lhs.terminal: return False
+        # if lhs.terminal: return False # we already assert this when reading the grammar
         if length not in (1, 2): return False
         if length == 1 and not rhs[0].terminal: return False
         if length == 2 and (rhs[0].terminal or rhs[1].terminal): return False
         return True
 
     def is_relaxedCNF(self):
-        return all([self._validate_relaxed_rule(rule) for rule in self.rules])
-    def _validate_relaxed_rule(self, rule: GrammarRule) -> bool:
+        return all([self._is_valid_relaxed_rule(rule) for rule in self.rules])
+
+    def _is_valid_relaxed_rule(self, rule: GrammarRule) -> bool:
         lhs, rhs, length = rule.lhs, rule.rhs, len(rule.rhs)
-        if lhs.terminal: return False
         if length not in (1, 2): return False
         if length == 2 and (rhs[0].terminal or rhs[1].terminal): return False
         return True
+
+    def normalize_rules(self):
+        normalized_rules = []
+        for rule in self.rules:
+            if self._is_valid_relaxed_rule(rule):
+                normalized_rules.append(rule)
+                continue
+            else:
+                lhs, rhs = rule.lhs, rule.rhs
+                new_rules: List[GrammarRule] = []
+                _rhs = []
+
+                for s in rhs:
+                    if not s.terminal:
+                        _rhs.append(s)
+                    else:
+                        new_s = Symbol('$_' + s.symbol.capitalize(), False)
+                        _rhs.append(new_s)
+                        new_rules.append(GrammarRule(new_s, [s]))
+
+                # if len(_rhs) % 2 == 1:
+                #     last_s = _rhs.pop()
+                #     new_rules.append(GrammarRule(Symbol('$_' + last_s.symbol, False), [last_s]))
+                #
+                # for i in range(0, len(_rhs), 2):
+                #     first, second = _rhs[i], _rhs[i + 1]
+                #     new_rules.append(GrammarRule(Symbol('$_' + first.symbol + second.symbol, False), [first, second]))
+
+                self.split_rule(rule, new_rules)
+
+                normalized_rules.extend(new_rules)
+
+        self.rules = normalized_rules
+
+    def split_rule(self, rule: GrammarRule, new_rules: List[GrammarRule]):
+        if len(rule.rhs) == 2:
+            new_rules.append(rule)
+        else:
+            left_part, right_part = rule.rhs[0], rule.rhs[1:]
+            new_sym_for_right_part = Symbol("$_" + "".join([sym.symbol for sym in right_part], False))
+            new_rules.append(GrammarRule(rule.lhs, [left_part, new_sym_for_right_part]))
+            self.split_rule(GrammarRule(new_sym_for_right_part, right_part))
+
